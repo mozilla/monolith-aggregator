5361368da002a3b8ce52d17e3ecc7ddec494b96d
Hanno Schlichting <hanno@hannosch.eu>
2013-01-16 16:18:25 +0100

    spell SQLAlchemy correctly in CamelCase

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
-requires = ['sqlalchemy',]
+requires = ['SQLAlchemy', ]
 test_requires = requires + ['unittest2', 'nose', 'coverage']
 
 setup(
