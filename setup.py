5c6382912ff9a9bc5991f3640de87c6422867839
Hanno Schlichting <hanno@hannosch.eu>
2013-01-16 15:42:12 +0100

    indent setup args to 4 spaces, and add `test` extra

--- setup.py
+++ setup.py
@@ -7,21 +7,29 @@ with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 requires = ['sqlalchemy', 'bson']
+test_requires = requires + ['unittest2', ]
 
-
-setup(name='monolith-aggregator',
-      version='0.1',
-      description='The monolith aggregator',
-      long_description=README,
-      classifiers=[
+setup(
+    name='monolith-aggregator',
+    version='0.1',
+    description='The monolith aggregator',
+    long_description=README,
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      author='Mozilla Services',
-      author_email='services-dev@mozilla.org',
-      url='https://github.com/mozilla/monolith-aggregator',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires + ['unittest2',],
-      test_suite="aggregator")
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    author='Mozilla Services',
+    author_email='services-dev@mozilla.org',
+    url='https://github.com/mozilla/monolith-aggregator',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require={
+        'test': test_requires,
+    },
+    tests_require=test_requires,
+    test_suite="aggregator",
+)
