e0aed5c7355ac30cdaf889ab7bf2a1af91d0afea
Tarek Ziade <tarek@ziade.org>
2013-01-22 13:04:11 +0100

    fixed option passing

--- aggregator/plugins/sqlwrite.py
+++ aggregator/plugins/sqlwrite.py
@@ -4,8 +4,8 @@ from aggregator.db import Database
 class SQLInjecter(object):
     """SQL"""
 
-    def __init__(self, database, **options):
-        self.db = Database(sqluri=database)
+    def __init__(self, **options):
+        self.db = Database(sqluri=option['database'])
 
     def __call__(self, data, **options):
         self.db.put(category='', **data)
