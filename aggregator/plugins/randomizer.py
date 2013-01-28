823cb972b86b2a68f836a2626e8ad752a0d1ff05
Alexis Métaireau <alexis@notmyidea.org>
2013-01-28 17:29:38 +0100

    We want to generate data for multiple apps.

--- aggregator/plugins/randomizer.py
+++ aggregator/plugins/randomizer.py
@@ -6,16 +6,21 @@ from aggregator.plugins import Plugin
 
 class RandomGenerator(Plugin):
 
+    def __init__(self, **options):
+        self.options = options
+
     def __call__(self, start_date, end_date, *args, **options):
         platforms = options.get('platforms')
+        addons = int(options.get('addons', 100))
         if platforms is None:
             platforms = ('Mac OS X', 'Windows 8', 'Ubuntu')
         else:
             platforms = platforms.split(', ')
 
-        for delta in range((end_date - start_date).days):
-            yield {'date': start_date + datetime.timedelta(days=delta),
-                   'os': random.choice(platforms),
-                   'downloads_count': random.randint(1000, 1500),
-                   'users_count': random.randint(10000, 15000),
-                   'add_on': 1}
+        for addon in range(addons):
+            for delta in range((end_date - start_date).days):
+                yield {'date': start_date + datetime.timedelta(days=delta),
+                       'os': random.choice(platforms),
+                       'downloads_count': random.randint(1000, 1500),
+                       'users_count': random.randint(10000, 15000),
+                       'add_on': addon + 1}
