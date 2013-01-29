66634990bd73919a51475500dd561261d71d3d85
Alexis Métaireau <alexis@notmyidea.org>
2013-01-29 18:08:25 +0100

    Get the type from the .ini file, and use it.

--- aggregator/plugins/metrics.py
+++ aggregator/plugins/metrics.py
@@ -9,12 +9,14 @@ class FileReader(Plugin):
 
     def __init__(self, parser, **options):
         metrics_config = dict(parser.items('metrics'))
+        self._options = options
+
         self._auth = (metrics_config['username'],
                       metrics_config['password'])
         self._baseurl = metrics_config['url']
         self._filename_format = options['filename_format']
-        self._options = options
         self._data_format = re.compile(options['data_format'])
+        self._type = options['type']
 
     def __call__(self, start_date, end_date):
         date = start_date
@@ -28,4 +30,9 @@ class FileReader(Plugin):
             date += datetime.timedelta(days=1)
 
     def _parse_data(self, content):
-        return (i.groupdict() for i in self._data_format.finditer(content))
+        def _get_item(data):
+            item = {'type': self._type}
+            item.update(data.groupdict())
+            return item
+
+        return (_get_item(d) for d in self._data_format.finditer(content))
