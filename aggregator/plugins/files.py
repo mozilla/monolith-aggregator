448e7bcd22396a36521e207d6af9f976bf28118f
Alexis Métaireau <alexis@notmyidea.org>
2013-01-29 17:55:55 +0100

    Add a simple file writer as a plugin.
    This can be sueful for debugging.

---
+++ aggregator/plugins/files.py
@@ -0,0 +1,13 @@
+from aggregator.plugins import Plugin
+from aggregator.util import json_dumps
+
+
+class FileWriter(Plugin):
+
+    def __init__(self, **options):
+        self._filename = options['filename']
+        self._file = open(self._filename, 'w+')
+
+    def __call__(self, batch):
+        for data in batch:
+            self._file.write(json_dumps(data))
