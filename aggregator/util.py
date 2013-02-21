a89f5df98805c52f17789bb18fba93bc40e7ef1f
Hanno Schlichting <hanno@hannosch.eu>
2013-02-21 22:24:47 +0100

    generate uuid's based on passed in date, so we preserve data locality when bulk loading / reindexing data for older date ranges

--- aggregator/util.py
+++ aggregator/util.py
@@ -7,7 +7,7 @@ import sys
 import logging
 import fcntl
 from datetime import date, datetime, timedelta
-from calendar import monthrange
+from calendar import monthrange, timegm
 import uuid
 
 try:
@@ -225,27 +225,32 @@ def word2daterange(datestr):
 
     raise NotImplementedError(datestr)
 
-_last_timestamp = 0
 _randrange = random.Random().randrange
 _node = uuid.getnode()
 
 
-def urlsafe_uuid(date=None):
+def urlsafe_uuid(_date=None):
     """
     A simplified version of uuid1 - optimized for usage as a
     MySQL primary key and ElasticSearch id.
     """
-    global _last_timestamp
-    timestamp = int(time() * 1e7) + 0x01b21dd213814000L
-    if timestamp <= _last_timestamp:  # pragma: no cover
-        timestamp = _last_timestamp + 1
-    _last_timestamp = timestamp
+    if _date is None:
+        _date = date.today()
+    # take the passed in date, and add the current time to it
+    # that way we preserve the right coarse-grained date, but also
+    # make duplicates less likely by having a non-constant time part
+    now = datetime.utcnow()
+    timestamp = timegm(_date.timetuple()[:3] + now.timetuple()[3:])
+    timestamp = int((timestamp * 1e6) + now.microsecond)
+    timestamp = (timestamp * 10) + 0x01b21dd213814000L
 
     time_low = timestamp & 0xffffffffL  # changes every 100 nano-seconds
     time_mid = (timestamp >> 32L) & 0xffffL  # changes every ~15 minutes
     time_hi = (timestamp >> 48L) & 0x0fffL  # changes every ~3 years
     clock_seq = _randrange(1 << 14L)
 
+    # order so we get a stable prefix per node and time, which helps
+    # data locality in BTree inserts
     int_ = ((_node << 80L) | (time_hi << 64L) | (time_mid << 48L) |
             (time_low << 16L) | clock_seq)
 
