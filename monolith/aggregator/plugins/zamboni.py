59cd2feaa5cf031cb7c5d6eba17db7c80060335f
Tarek Ziade <tarek@ziade.org>
2013-04-15 14:06:36 +0200

    let's keep every key we get - summing or overwriting depending on their type

--- monolith/aggregator/plugins/zamboni.py
+++ monolith/aggregator/plugins/zamboni.py
@@ -26,6 +26,18 @@ class APIReader(TastypieReader):
             res = self.delete(self.endpoint, params=params)
             res.raise_for_status()
 
+    def _update_fields(self, fields, new_values):
+        for key, value in new_values.items():
+            if key not in fields:
+                fields[key] = value
+            else:
+                if isinstance(value, (int, float)):
+                    # sum
+                    fields[key] += value
+                else:
+                    # overwrite
+                    fields[key] = value
+
     def extract(self, start_date, end_date):
         end_date = end_date + timedelta(days=1)
 
@@ -35,24 +47,31 @@ class APIReader(TastypieReader):
             'recorded__lte': end_date.isoformat()})
 
         # building counts grouped by date & dimensions
-        results = defaultdict(int)
+        results = {}
 
         for item in data:
             timestamp = iso2datetime(item['recorded'])
             day = date(timestamp.year, timestamp.month, timestamp.day)
+
             values = item.pop('value')
+            values['_date'] = day
             key = [('_date', day)]
+
             for dimension in self.dimensions:
                 if dimension in values:
                     key.append((dimension, values[dimension]))
+
             key.sort()
             key = tuple(key)
 
-            results[key] += 1
+            if key not in results:
+                results[key] = values
+                results[key]['_type'] = self.type
+                results[key][self.field] = 1
+            else:
+                self._update_fields(results[key], values)
+                results[key][self.field] += 1
 
         # rendering the result
-        for key, count in results.items():
-            line = {'_type': self.type, self.field: count}
-            for field, value in key:
-                line[field] = value
+        for line in results.values():
             yield line
