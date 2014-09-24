e67fb9c3997db0d3ab85a8703d3d550c5fcf0716
Jason Thomas <jason@lithiumfox.com>
2014-09-24 15:04:25 -0400

    add reindexing commands to fabfile.

--- fabfile.py
+++ fabfile.py
@@ -46,3 +46,64 @@ def update():
     with lcd(MONOLITH):
         local('%s setup.py develop --no-deps' % PYTHON)
         local('%s /usr/bin/virtualenv --relocatable %s' % (PYTHON, VIRTUALENV))
+
+
+@task
+def reindex(startdate):
+    delete_indices(startdate)
+    delete_records(startdate)
+    index_dates(startdate)
+
+
+@task
+def delete_indices(startdate):
+    from datetime import datetime, date
+
+    ES_URL = config.get('target:es', 'url')
+    ES_PREFIX = config.get('target:es', 'prefix')
+
+    today = date.today()
+    previous = datetime.strptime(startdate, '%Y-%m-%d')
+
+    # delete old indicies
+    for month in range(previous.month, today.month + 1):
+        local('curl -XDELETE %s/%stime_%s-%s' %
+              (ES_URL, ES_PREFIX, today.year, "%02d" % (month,)))
+
+
+@task
+def delete_records(startdate):
+    from datetime import datetime, date
+    from sqlalchemy import create_engine, MetaData, Table
+
+    db_uri = config.get('monolith', 'history')
+    engine = create_engine(db_uri, echo=False)
+    metadata = MetaData()
+
+    today = date.today()
+
+    record = Table('record', metadata, autoload=True, autoload_with=engine)
+    transaction = Table('monolith_transaction', metadata,
+                        autoload=True, autoload_with=engine)
+
+    delete_record = record.delete(record.c.date.between(startdate, today))
+    delete_transaction = transaction.delete(transaction.c.date.between(startdate, today))
+    engine.execute(delete_record)
+    engine.execute(delete_transaction)
+
+
+@task
+def index_dates(startdate):
+    from datetime import date, datetime, timedelta
+
+    today = date.today()
+    previous = datetime.strptime(startdate, '%Y-%m-%d').date()
+
+    delta = today - previous
+
+    for i in range(delta.days + 1):
+        rundate = previous + timedelta(days=i)
+        with lcd(MONOLITH):
+            local('%s ../venv/bin/monolith-extract aggregator.ini '
+                  '--log-level debug --start-date %s '
+                  '--end-date %s' % (PYTHON, rundate, rundate))
